c0f94f35652d42082b65acae1bac1e8ac5d14cd8
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-06 17:14:05 +0300

    PersonBuilder.java

---
+++ src/main/java/PersonBuilder.java
@@ -0,0 +1,14 @@
+public class PersonBuilder {
+
+    //todo заполнить корректно класс builder
+
+    public PersonBuilder setName(String name) { /*...*/ }
+
+    public PersonBuilder setSurname(String surname) { /*...*/ }
+
+    public PersonBuilder setAge(int age) { /*...*/ }
+
+    public PersonBuilder setAddress(String address) { /*...*/ }
+
+    public Person build() { /*...*/ }
+}
