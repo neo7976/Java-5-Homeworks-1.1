08205fc1ff8a27af2c70616ec6a941ead223a213
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-06 17:20:44 +0300

    PersonBuilder.java - Ещё пишется

--- src/main/java/PersonBuilder.java
+++ src/main/java/PersonBuilder.java
@@ -1,10 +1,21 @@
 public class PersonBuilder {
+    private String name;
+    private String surname;
+    private int age;
+    private String address;
 
     //todo заполнить корректно класс builder
 
-    public PersonBuilder setName(String name) { /*...*/ }
 
-    public PersonBuilder setSurname(String surname) { /*...*/ }
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
 
     public PersonBuilder setAge(int age) { /*...*/ }
 
