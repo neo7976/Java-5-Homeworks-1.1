dc3de59b1a145dde94c6c9e7c3a15df3c7fdebea
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-07 13:26:12 +0300

    Готовое ДЗ. Написание тестов

--- src/test/java/homework/PersonTest.java
+++ src/test/java/homework/PersonTest.java
@@ -2,9 +2,14 @@ package homework;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.OptionalInt;
+import java.util.stream.Stream;
 
 class PersonTest {
     Person person;
@@ -25,7 +30,35 @@ class PersonTest {
     }
 
     @Test
-    void getName() {
+    void getName_test() {
         Assertions.assertEquals("Дмитрий", person.getName());
     }
+
+    public static Stream<Arguments> sourceAge() {
+        return Stream.of(
+                Arguments.of(27),
+                Arguments.of(28),
+                Arguments.of(0),
+                Arguments.of(10)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("sourceAge")
+    void getAge_test(int expected) {
+        Assertions.assertEquals(expected, person.setAge(expected));
+    }
+
+    @Test
+    void happyBirthday_test() {
+        person.happyBirthday();
+        Assertions.assertEquals(OptionalInt.of(28), person.getAge());
+    }
+
+    @Test
+    void ageException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> person.setAge(-5));
+        Assertions.assertDoesNotThrow(()->person.setAge(10));
+    }
+
 }
